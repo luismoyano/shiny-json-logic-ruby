1fbd5fb727b8f8006c6201184471edbad1a9e36a
Luis Moyano <luis@praxeo.app>
2023-01-08 23:24:56 +0100

    Missing some is working

---
+++ lib/shiny_json_logic/operations/missing_some.rb
@@ -0,0 +1,15 @@
+require "shiny_json_logic/truthy"
+require "shiny_json_logic/operations/missing"
+
+module ShinyJsonLogic
+  module Operations
+    class MissingSome < Missing
+      def call
+        return rules[1] unless data.is_a?(Hash) && rules.is_a?(Array)
+
+        present = rules[1] & data.keys
+        present.size >= rules[0] ? [] : Missing.new(v[1], d).call
+      end
+    end
+  end
+end
