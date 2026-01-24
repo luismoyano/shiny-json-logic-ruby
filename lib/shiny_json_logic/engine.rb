363e2f7230363353db7fc53723be0ef99b60a4c0
Luis Moyano <moyano@hey.com>
2026-01-24 21:21:55 +0100

    Moving the operations to an iterative pattern to avoid recursion issues

--- lib/shiny_json_logic/engine.rb
+++ lib/shiny_json_logic/engine.rb
@@ -12,25 +12,72 @@ module ShinyJsonLogic
     end
 
     def call(rule = self.rule, data = self.data)
-      if rule.is_a?(Hash)
-        operation, raw_args = rule.to_a.first
-        if collection_solvers.key?(operation)
-          collection_solvers.fetch(operation).new(raw_args, data).call
-        else
-          evaluated_args =
-            if raw_args.is_a?(Array)
-              raw_args.map { |val| call(val, data) }
+      stack   = [[rule, data, :enter]]
+      results = []
+
+      while stack.any?
+        node, ctx, state = stack.pop
+
+        if state == :exit
+          # Already evaluated children, we solve
+          if node.is_a?(Hash)
+            op, raw_args = node.to_a.first
+
+            if collection_solvers.key?(op)
+              results << collection_solvers.fetch(op).new(raw_args, ctx).call
             else
-              Array.wrap(call(raw_args, data))
+              if raw_args.is_a?(Array)
+                argc = raw_args.size
+                args = results.pop(argc)
+                results << solvers.fetch(op).new(args, ctx).call
+              else
+                child = results.pop
+
+                args =
+                  if child.nil?
+                    []
+                  elsif child.is_a?(Array)
+                    child
+                  else
+                    [child]
+                  end
+
+                results << solvers.fetch(op).new(args, ctx).call
+              end
             end
 
-          solvers.fetch(operation).new(evaluated_args, data).call
+          elsif node.is_a?(Array)
+            results << results.pop(node.size)
+          end
+
+          next
         end
-      elsif rule.is_a?(Array)
-        rule.map { |item| call(item, data) }
-      else
-        rule
+
+        # ENTER phase
+        case node
+        when Hash
+          op, raw_args = node.to_a.first
+
+          stack << [node, ctx, :exit]
+
+          unless collection_solvers.key?(op)
+            if raw_args.is_a?(Array)
+              raw_args.reverse_each { |a| stack << [a, ctx, :enter] }
+            else
+              stack << [raw_args, ctx, :enter]
+            end
+          end
+
+        when Array
+          stack << [node, ctx, :exit]
+          node.reverse_each { |n| stack << [n, ctx, :enter] }
+
+        else
+          results << node
+        end
       end
+
+      results.last
     end
 
     private
