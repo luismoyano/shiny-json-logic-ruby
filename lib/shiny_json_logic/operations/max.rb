5e9ed24878fe5ea73828a382f16c5af5d6b271b7
Luis Moyano <moyano@hey.com>
2026-02-08 00:08:22 +0100

    refactor: extract shared logic from max/min into MinMaxCollection module

--- lib/shiny_json_logic/operations/max.rb
+++ lib/shiny_json_logic/operations/max.rb
@@ -1,16 +1,13 @@
 require "shiny_json_logic/operations/base"
+require "shiny_json_logic/numericals/min_max_collection"
 
 module ShinyJsonLogic
   module Operations
     class Max < Base
+      include Numericals::MinMaxCollection
+
       def call
-        result = nil
-        Array.wrap_nil(rules).each do |rule|
-          Array.wrap_nil(evaluate(rule)).each do |val|
-            result = val if result.nil? || val > result
-          end
-        end
-        result
+        collect_numeric_values.max
       end
     end
   end
